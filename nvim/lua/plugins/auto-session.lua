86b8af590491cd91be0a0bcfec78d5f82df335dd
Luca Pasini <luca.pasini@supsi.ch>
2024-05-31 11:43:30 +0200

    feat: auto-session management for projects
    feat: debugging (only TS/JS for now)
    
    feat: added custom config module not committed

---
+++ nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+	"rmagatti/auto-session",
+    dependencies = {
+        "nvim-neo-tree/neo-tree.nvim",
+    },
+	config = function()
+		require("auto-session").setup({
+			log_level = "error",
+			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+            post_restore_cmds = {
+                "Neotree"
+            },
+            pre_save_cmds = {
+                "tabdo Neotree close"
+            },
+		})
+	end,
+}
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,5 +3,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set autoindent")
 vim.g.mapleader = " "
 
+
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
