14e223dea3b3cbb77a561d8a3589de73ce84c7f3
Pasini Luca <luca.pasini@supsi.ch>
2024-06-07 09:16:52 +0200

    style: terminal formatting

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,38 +1,40 @@
 --- Ctrl+/: Toggle terminal
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			open_mapping = [[<C-_>]],
-			on_open = function()
-				vim.cmd("setlocal nospell")
-			end,
-		})
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+        require("toggleterm").setup({
+            open_mapping = [[<C-_>]],
+            on_open = function()
+                vim.cmd("setlocal nospell")
+            end,
+        })
 
-		-- keymaps for lazygit
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({
-			cmd = "lazygit",
-			direction = "float",
-			hidden = true,
-		})
+        -- exit terminal before saving session
 
-		function _Lazygit_toggle()
-			lazygit:toggle()
-		end
+        -- keymaps for lazygit
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new({
+            cmd = "lazygit",
+            direction = "float",
+            hidden = true,
+        })
 
-		vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>lua _Lazygit_toggle()<CR>", {
-			noremap = true,
-			silent = true,
-		})
+        function _Lazygit_toggle()
+            lazygit:toggle()
+        end
 
-		-- keymaps for lazydocker
-		local lazydocker = Terminal:new({
-			cmd = "lazydocker",
-			direction = "float",
-			hidden = true,
-		})
+        vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>lua _Lazygit_toggle()<CR>", {
+            noremap = true,
+            silent = true,
+        })
+
+        -- keymaps for lazydocker
+        local lazydocker = Terminal:new({
+            cmd = "lazydocker",
+            direction = "float",
+            hidden = true,
+        })
 
         function _Lazydocker_toggle()
             lazydocker:toggle()
@@ -42,5 +44,14 @@ return {
             noremap = true,
             silent = true,
         })
-	end,
+
+        -- keymaps to switch between windows
+        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end,
 }
