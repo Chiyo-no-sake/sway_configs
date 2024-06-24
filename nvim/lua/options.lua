287d4e4216e420c5b6ad577dfcabc4ab6c11446f
Pasini Luca <luca.pasini@supsi.ch>
2024-06-24 08:23:01 +0200

    feat: add vimtex

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,12 +6,16 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set autoindent")
 vim.g.mapleader = " "
 
+vim.cmd("set nosmd")
+vim.cmd("set noru")
+
 -- TODO: not working, need to investigate
 -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]])
 -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]])
 -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]])
 -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]])
 
+vim.keymap.set({"n", "t"}, "<leader>zz", "vim.cmd('wa') | vim.cmd('qa')", { noremap = true })
 
 -- Set the sessionoptions
 -- required for debugging with DAP
