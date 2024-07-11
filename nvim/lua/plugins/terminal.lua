e9397ce0c530e793fa206037a3f802673da2e053
Pasini Luca <luca.pasini@supsi.ch>
2024-07-11 14:23:19 +0200

    feat: add surround for brackets, leap navigation, some textobjects

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,56 +1,69 @@
 --- Ctrl+/: Toggle terminal
 return {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-        require("toggleterm").setup({
-            open_mapping = [[<C-_>]],
-            on_open = function()
-                vim.cmd("setlocal nospell")
-            end,
-        })
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<C-`>]],
+			on_open = function()
+				vim.cmd("setlocal nospell")
+			end,
+		})
 
-        -- exit terminal before saving session
+		-- exit terminal before saving session
 
-        -- keymaps for lazygit
-        local Terminal = require("toggleterm.terminal").Terminal
-        local lazygit = Terminal:new({
-            cmd = "lazygit",
-            direction = "float",
-            hidden = true,
-        })
+		-- keymaps for lazygit
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			direction = "float",
+			hidden = true,
+		})
 
-        function _Lazygit_toggle()
-            lazygit:toggle()
-        end
+		function _Lazygit_toggle()
+			lazygit:toggle()
+		end
 
-        vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>lua _Lazygit_toggle()<CR>", {
-            noremap = true,
-            silent = true,
-        })
+		vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>lua _Lazygit_toggle()<CR>", {
+			noremap = true,
+			silent = true,
+		})
 
-        -- keymaps for lazydocker
-        local lazydocker = Terminal:new({
-            cmd = "lazydocker",
-            direction = "float",
-            hidden = true,
-        })
+		-- keymaps for lazydocker
+		local lazydocker = Terminal:new({
+			cmd = "lazydocker",
+			direction = "float",
+			hidden = true,
+		})
 
-        function _Lazydocker_toggle()
-            lazydocker:toggle()
-        end
+		function _Lazydocker_toggle()
+			lazydocker:toggle()
+		end
 
-        vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua _Lazydocker_toggle()<CR>", {
-            noremap = true,
-            silent = true,
-        })
+		vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>lua _Lazydocker_toggle()<CR>", {
+			noremap = true,
+			silent = true,
+		})
 
-        -- keymaps to switch between windows
-        vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-    end,
+		vim.api.nvim_create_autocmd({ "TermEnter" }, {
+			callback = function()
+				for _, buffers in ipairs(vim.fn.getbufinfo()) do
+					local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, "filetype")
+					if filetype == "toggleterm" then
+						vim.api.nvim_create_autocmd({ "BufWriteCmd", "FileWriteCmd", "FileAppendCmd" }, {
+							buffer = buffers.bufnr,
+							command = "q!",
+						})
+					end
+				end
+			end,
+		})
+		-- keymaps to switch between windows
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+		vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+		vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+		vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+		vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+		vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+	end,
 }
